9f8b77e97fa9393276bcce6d44bb87735996e32e
Ashley Knowles <knowlesa@uit-knowlesa-d2.ad.uwm.edu>
2013-04-18 10:53:28 -0500

    Final restore script.
    Made changes so that it works as it should.

--- restoretestscript.sh
+++ restoretestscript.sh
@@ -3,39 +3,26 @@
 # Set Path to internal drive
 export DS_INTERNAL_DRIVE=`system_profiler SPSerialATADataType | awk -F': ' '/Mount Point/ { print $2}'|head -n1`
 
-# Unique ID for plist and common variable for scripts
-#export UNIQUE_ID=`echo "$DS_PRIMARY_MAC_ADDRESS"|tr -d ':'`
-
-
-# DS Script to backup user data with tar to Backups folder on repository.
-#export DS_REPOSITORY_BACKUPS="$DS_REPOSITORY_PATH/Backups/$input_variable"
-
-#sets path to CocoaDialog
-CD="$HOME/CocoaDialog.app/Contents/MacOS/CocoaDialog"
-
-ui=`$CD standard-inputbox --title "Please Enter a Value" \
---informative-text "Please enter a Unique ID" \
---no-newline --float`
-
 UNIQUE_ID=`echo $ui | awk '{ print $2 }'`
 
 #Set Path to the shared folder
 export DS_SHARED_PATH="/Users/Shared"
 
-#This should copy the backups to the local drive's shared folder. 
-#cp -R $DS_REPOSITORY_BACKUPS "$DS_INTERNAL_DRIVE$DS_SHARED_PATH/"
+#sets path to CocoaDialog
+CD="/Applications/Utilities/CocoaDialog.app/Contents/MacOS/CocoaDialog"
 
-dropdown=`$CD standard-dropdown --title "Choose a Backup" --string-output --no-newline --text "Please choose a baskup to restore" --items "ls ~/Backup" `
+#sets path to the backup folder in deploystudio.
+u=`ls $DS_REPOSITORY_PATH/Backups/ | grep -v ".DS_Store"`
 
+#dropdown that lists all folders in the backup folder.
+dropdown=`$CD standard-dropdown --title "Choose a Backup" --string-output --no-newline --text "Please choose a baskup to restore" --items $u `
+
+#get the backup variable that is chosen.
 backup=`echo $dropdown | awk '{ print $2 }'`
 
-if [ "$backup" = "$UNIQUE_ID" ]; then
-	export DS_REPOSITORY_BACKUPS="$DS_REPOSITORY_PATH/Backups/$rv"
-	cp -R $DS_REPOSITORY_BACKUPS "$DS_INTERNAL_DRIVE$DS_SHARED_PATH/"
-else
-	echo "There is no match"
-	exit 1
-fi
-
+#This should set what the backup folder is so it can be copied.
+export DS_REPOSITORY_BACKUPS="$DS_REPOSITORY_PATH/Backups/$backup"
+#This should copy the backups to the local drive's shared folder. 
+cp -R $DS_REPOSITORY_BACKUPS "$DS_INTERNAL_DRIVE$DS_SHARED_PATH/"
 
 exit 0
