a5fbcb2823383b9bfcdd53bc58fe4911a544c011
Kyle Brockman <kyle@Kyles-Retina-MBPro.local>
2013-10-13 12:38:36 -0500

    Added new backup script.
    This script will be started by the client. It will e-mail tech support
    staff when it's done.

---
+++ UITS-SelfServiceMachineBackup copy.sh
@@ -0,0 +1,112 @@
+#!bin/bash
+
+###
+#Script Designed by Ian Gunther and Kyle Brockman
+#While working at the University of Wisconsin-Milwaukee
+###
+
+export TIME=`date "+%Y.%m.%d"`
+
+export FULLTIME=`date "+%Y.%m.%d %H:%M:%S"`
+
+export BACKUP=$TIME-`hostname`
+
+export M="MacBackups"
+
+export bkVolume="/tmp/$M"
+
+# create a named pipe
+rm -f /tmp/hpipe
+mkfifo /tmp/hpipe
+
+# create a background job which takes its input from the named pipe
+/Applications/Utilities/CocoaDialog.app/Contents/MacOS/CocoaDialog progressbar \
+--indeterminate --title "Machine Backup For UITS Desktop Support" \
+--text "Backing Up..." < /tmp/hpipe &
+
+# associate file descriptor 3 with that pipe and send a character through the pipe
+exec 3<> /tmp/hpipe
+echo -n . >&3
+
+#Stop the machine from sleeping for 24 Hours
+caffeinate &
+
+#Make the mount point if it does not already present.
+if [[ ! -d "/tmp/$M" ]]; then
+	mkdir "/tmp/$M"
+fi
+
+#Umount anything that might be mounted to the backup point 
+umount /tmp/$M
+
+sleep 10
+
+#mount the backup share
+mount_smbfs //server.local/$M /tmp/$M
+
+sleep 10
+
+
+if mount | grep $bkVolume; then
+	echo "SAMBA is Mounted"
+else
+	echo "Samba is NOT MOUNTED"
+	# now turn off the progress bar by closing file descriptor 3
+	exec 3>&-
+	# wait for all background jobs to exit
+	wait
+	rm -f /tmp/hpipe
+	exit 1
+fi
+
+#make the backup of the drive
+hdiutil create /tmp/MacBackups/AutoDelete/$BACKUP.dmg -verbose -format UDBZ -nocrossdev -srcfolder /
+
+#scan the image for restore
+asr imagescan --source $BACKUP --verbose
+
+#check the backup
+hdiutil verify /tmp/MacBackups/AutoDelete/$BACKUP.dmg
+
+if [[ $? == 0 ]]; then
+	echo "Backup is good"
+else
+	mail -s "Backup Failed for `hostname` at `date "+%Y.%m.%d %H:%M:%S"`" ds-techs@uwm.edu ds-fte@uwm.edu <<EOF
+
+	Hello,
+
+	The backup for `hostname` failed.
+
+	Thank you,
+	Backup Process.
+
+EOF
+
+fi
+# now turn off the progress bar by closing file descriptor 3
+exec 3>&-
+
+# wait for all background jobs to exit
+wait
+rm -f /tmp/hpipe
+
+#E-mail Techs the backup is completed. 
+
+mail -s "Backup Complete for `hostname` at `date "+%Y.%m.%d %H:%M:%S"`" ds-techs@uwm.edu ds-fte@uwm.edu <<EOF
+
+Hello,
+
+The backup for `hostname` is complete and ready to be picked up to be re-imaged.
+
+It started at `echo $FULLTIME` and ended at `date "+%Y.%m.%d %H:%M:%S"`.
+
+Total backup size is `ls -lah /tmp/MacBackups/AutoDelete/$BACKUP.dmg | awk '{ print $5 }'`.
+
+Thank you,
+Backup Process.
+
+EOF
+
+killall caffeinate
+
+exit 0
