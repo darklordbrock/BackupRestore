7ff7210474c45922c4bf8ba9e3b9dc2b37915dfc
Kyle Brockman <kyle@Kyles-Retina-MBPro.local>
2013-10-21 14:47:37 -0500

    Added the restore code
    Added the restore and added more comments.

--- UITS-RestoreFromSelfService.sh
+++ UITS-RestoreFromSelfService.sh
@@ -6,6 +6,8 @@ export bkVolume="/tmp/$M"
 
 export serial=`system_profiler | grep "Serial Number (system)" | awk '{print $4}'`
 
+export restore="/tmp/restore"
+
 #Stop the machine from sleeping
 caffeinate &
 
@@ -42,8 +44,69 @@ EOF
 	exit 1	
 fi
 
+#####
+#check if connected to AD
+#####
+
+ADWORK=`id brockma9`
+if [[ "$ADWORK" == "id: brockma9: no such user" ]]; then
+	echo "This machine is not connected to AD"
+	exit 1
+else
+	echo "Connected to AD and moving on. Nothing to see here, move along."
+fi
+
+#####
+#end of check if connected to AD
+#####
+
 ######
 #code to restore the user dirs
+######
+
+mkdir $restore
+
+#attach the backup
+hdiutil attach $bkVolume/AutoDelete/$serial* -mountpoint $restore
+
+USERZ=`ls $restore/Users/* | grep -v ".localized" | grep -v "localadmin"`
+
+for U in $USERZ; do
+	#Making the new home folder for the AD user account
+	/System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $U
+	/usr/sbin/createhomedir -c -u $U
+	sleep 2
+	
+	if [[ ! -d "/Users/$U" ]]; then
+		echo "Home Directory failed to be made."
+		exit 1
+	else
+		
+		#copy the files to the new home folder 
+		FILEZ=`ls -a $restore/Users/$U/`
+		for F in $FILEZ; do
+			#Copy the files into the home dir
+			cp -Rfp $restore/Users/$U/$F /Users/$U/			
+		done
+		
+		#Set Permissions on the home folders
+		for P in `ls /Users/ | grep -v Shared | grep -v localadmin` ; do
+			#sets owner and group to user and staff
+			chown -R $P:staff /Users/$P
+			#sets the folder to let the staff users in but no access
+			chmod 740 /Users/$P
+			#sets the user to have full access to the home dir and no one else.
+			chmod -R 700 /Users/$P/*
+			#Access to the public folder
+			chmod 777 /Users/$P/Public
+		done 
+	fi		
+done	
+
+hdiutil detach $restore
+
+######
+#code of code to restore the user dirs
 ######
 
 killall caffeinate
