94297b255f10569493a1f8a8dacadc9d76bb4d07
Kyle Brockman <kyle@Kyles-Retina-MBPro.local>
2013-11-19 12:54:47 -0600

    Cleaned up to do less
    Removed a lot from the first boot script for speed and cleaner process.

--- firstBootAfterJSS.04.01.2013.sh
+++ firstBootAfterJSS.04.01.2013.sh
@@ -4,13 +4,11 @@
 # First boot script for JSS Production 
 # After JSS QuickAdd is run. 
 # Built on 03.27.2013
-#
+# Updated on 2013.11.19
+# 
 # Kyle Brockman
 # While working at UW-Milwaukee
 ######
-
-# Add the jssworker account to local UWM admin group
-dscl . append /Groups/localuwmadmingroup GroupMembership "jss"
 
 #check for JAMF binary 
 if [[  -f /usr/sbin/jamf ]]; then
@@ -20,50 +18,7 @@ else
 	exit 1	
 fi
 
-#Set the department in JSS based on machine name
-DEPT=`hostname | sed 's/-/ /g' | awk '{print $1}' | tr "a-z" "A-Z"`
-echo "Setting Department to: " $DEPT
-/usr/sbin/jamf recon -department $DEPT
-
-#check for any jamf policies for the machine. 
-echo "check for any jamf policies"
-/usr/sbin/jamf policy
-sleep 3
-
-# Trigger manual Printer installs
-/usr/sbin/jamf policy -trigger printdriverBrother
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverCanon
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverEpson
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverFuji
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverGestetner
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverHP
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverInfo
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverInfoTec
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverLanier
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverLexmark
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverNRG
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverRicoh
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverSamsung
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverSavin
-sleep 2
-/usr/sbin/jamf policy -trigger printdriverXerox
-sleep 2
-
-# Restore user data from Shared dir if a backup is there
-
-/usr/sbin/jamf policy -trigger homedirmigration
+# Add the jssworker account to local UWM admin group
+dscl . append /Groups/localuwmadmingroup GroupMembership "jss"
 
 exit 0
